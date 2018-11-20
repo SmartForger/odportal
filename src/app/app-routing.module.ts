be8208c8561b5068a9d61afe48f76f96797986a5
Travis Larson <admin@travislarson.com>
2018-11-20 14:29:38 -0500

    new routes and modules for portal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  	path: 'portal',
+  	loadChildren: './modules/portal/portal.module#PortalModule'
+  }	
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/portal/sidebar-widgets/sidebar-widgets.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-widgets',
+  templateUrl: './sidebar-widgets.component.html',
+  styleUrls: ['./sidebar-widgets.component.scss']
+})
+export class SidebarWidgetsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
