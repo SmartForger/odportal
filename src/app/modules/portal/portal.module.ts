0b63a412fa8953d47db8abbcac208f057d0a9d3b
Steven Chambers <steven.chambers@ultimateknowledge.com>
2018-11-20 15:17:00 -0500

    Widget & Role Slide Out Menus

--- src/app/modules/portal/portal.module.ts
+++ src/app/modules/portal/portal.module.ts
@@ -9,6 +9,9 @@ import { SidebarRolesComponent } from './sidebar-roles/sidebar-roles.component';
 import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
 import { SidebarUserComponent } from './sidebar-user/sidebar-user.component';
 import { SidebarWidgetsComponent } from './sidebar-widgets/sidebar-widgets.component';
+import { SidebarRoleDockComponent } from './sidebar-role-dock/sidebar-role-dock.component';
+import { SidebarWidgetDockComponent } from './sidebar-widget-dock/sidebar-widget-dock.component';
+
 
 const ROUTES: Routes = [
   {
@@ -18,7 +21,18 @@ const ROUTES: Routes = [
 ];
 
 @NgModule({
-  declarations: [MainComponent, FooterComponent, SidebarLogoComponent, SidebarRolesComponent, SidebarMenuComponent, SidebarUserComponent, SidebarWidgetsComponent],
+  declarations: [
+    MainComponent, 
+    FooterComponent, 
+    SidebarLogoComponent, 
+    SidebarRolesComponent, 
+    SidebarMenuComponent, 
+    SidebarUserComponent, 
+    SidebarWidgetsComponent, 
+    SidebarRoleDockComponent, 
+    SidebarWidgetDockComponent
+  ],
+
   imports: [
     CommonModule,
     RouterModule.forChild(ROUTES)
