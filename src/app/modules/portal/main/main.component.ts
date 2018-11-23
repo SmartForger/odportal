b7829ab3a96018ccbb9314fc23057f35370d97f2
Steven Chambers <steven.chambers@ultimateknowledge.com>
2018-11-23 09:35:01 -0500

    Fix portal/main/main.component.html back to original

--- src/app/modules/portal/main/main.component.ts
+++ src/app/modules/portal/main/main.component.ts
@@ -2,31 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import {FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+ selector: 'app-main',
+ templateUrl: './main.component.html',
+ styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
 
-  formGroup: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor() { }
 
   ngOnInit() {
-    this.buildForm();
-  }
-
-  private buildForm(): void {
-    this.formGroup = this.formBuilder.group({
-      textInput: new FormControl('', [Validators.required, Validators.maxLength(250)]),
-      checkboxInput: new FormControl(false),
-      numberInput: new FormControl('', [Validators.required, Validators.max(250), Validators.min(0)]),
-      textareaInput: new FormControl(),
-      selectInput: new FormControl()
-    });
+    
   }
 
 }
-
-
-
