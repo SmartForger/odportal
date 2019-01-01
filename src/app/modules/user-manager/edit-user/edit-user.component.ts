31c18bc95eb9e694f7a47495d86b720e2dfb16ad
Steven M. Redman <steven.m.redman@gmail.com>
2018-12-31 21:20:42 -0800

    Users can now be enabled/disabled in User Management.

--- src/app/modules/user-manager/edit-user/edit-user.component.ts
+++ src/app/modules/user-manager/edit-user/edit-user.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {UserProfile} from '../../../models/user-profile.model';
 import {UsersService} from '../../../services/users.service';
 import {Subscription} from 'rxjs';
 import {EditBasicInfoComponent} from '../edit-basic-info/edit-basic-info.component';
 import {AjaxProgressService} from '../../../ajax-progress/ajax-progress.service';
+import {ModalComponent} from '../../display-elements/modal/modal.component';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.scss']
 })
-export class EditUserComponent implements OnInit {
+export class EditUserComponent implements OnInit, OnDestroy {
 
   user: UserProfile;
   private userSub: Subscription;
 
   @ViewChild(EditBasicInfoComponent) private basicInfo: EditBasicInfoComponent;
+  @ViewChild('deleteModal') private deleteModal: ModalComponent;
+  @ViewChild('enableModal') private enableModal: ModalComponent;
+  @ViewChild('disableModal') private disableModal: ModalComponent;
 
   constructor(
     private route: ActivatedRoute, 
@@ -25,6 +29,47 @@ export class EditUserComponent implements OnInit {
 
   ngOnInit() {
     this.fetchUser();
+    this.subscribeToUserUpdates();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
+
+  enableButtonClicked(enable: boolean): void {
+    this.showEnableOrDisableModal(enable);
+  }
+
+  enableConfirmed(btnText: string, enable: boolean): void {
+    this.ajaxSvc.show();
+    this.hideEnableOrDisableModal(enable);
+    this.user.enabled = enable;
+    this.usersSvc.updateProfile(this.user).subscribe(
+      (response: any) => {
+        this.ajaxSvc.hide();
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
+
+  private showEnableOrDisableModal(enable: boolean): void {
+    if (enable) {
+      this.enableModal.show = true;
+    }
+    else {
+      this.disableModal.show = true;
+    }
+  }
+
+  private hideEnableOrDisableModal(enable: boolean): void {
+    if (enable) {
+      this.enableModal.show = false;
+    }
+    else {
+      this.disableModal.show = false;
+    }
   }
 
   private fetchUser(): void {
@@ -37,6 +82,14 @@ export class EditUserComponent implements OnInit {
       },
       (err: any) => {
         console.log(err);
+      }
+    );
+  }
+
+  private subscribeToUserUpdates(): void {
+    this.userSub = this.usersSvc.userSubject.subscribe(
+      (user: UserProfile) => {
+        this.user = user;
       }
     );
   }
