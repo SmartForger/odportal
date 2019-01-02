108f9f8eefb5c2f5f3bee4d5e59d1739d129a5d6
Steven Chambers <steven.chambers@ultimateknowledge.com>
2019-01-02 11:12:20 -0500

    Log Manager Initial Layout

---
+++ src/app/modules/log-manager/log-manager.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogManagerComponent } from './log-manager.component';
+
+describe('LogManagerComponent', () => {
+  let component: LogManagerComponent;
+  let fixture: ComponentFixture<LogManagerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LogManagerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogManagerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
