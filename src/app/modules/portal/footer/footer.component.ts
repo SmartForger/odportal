3699595b03e0166f447a7dca6d6b0c5a208ea499
Steven Chambers <steven.chambers@ultimateknowledge.com>
2019-01-16 14:29:40 -0500

    Initial Help Popout-UI

--- src/app/modules/portal/footer/footer.component.ts
+++ src/app/modules/portal/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -10,6 +12,69 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSlideoutHelp(): void {
+    $('#slideout_innerHelp').toggleClass('expanded');
+  }
+
+  ngAfterViewInit() {
+    this.pageWrapperCollapseHelp(); 
+    this.logoAreaHelp(); 
+    this.overflowYHelp(); 
+    this.rmfWidgetsBgHelp(); 
+    this.closeWidget(); 
+    this.btnCloseSlider(); 
+    this.btnCloseBlock();
+    this.btnCloseFooterDock();
+  }
+
+  private pageWrapperCollapseHelp(): void {
+    $('#page-wrapper').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
+  }
+
+  private logoAreaHelp(): void {
+    $('.logo-area').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
+  }
+
+  private overflowYHelp(): void {
+    $('.overflow-y').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
+  }
+
+  private rmfWidgetsBgHelp(): void {
+    $('.footer-icon-bg').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
+  }
+
+  private closeWidget(): void {
+    $('.desktop-only-role').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
+  }
+
+  private btnCloseSlider(): void {
+    $('.btnCloseSlider').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
+  }
+
+  private btnCloseBlock(): void {
+    $('.user-block').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
+  }
+
+  private btnCloseFooterDock(): void {
+    $('.footer-dock-widget').click(() => {
+      $('#slideout_innerHelp').removeClass('expanded');
+    });
   }
 
 }
