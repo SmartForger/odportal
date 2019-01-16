213268564bdeaa663808b06da50ee9ce1762493c
Steven M. Redman <steven.m.redman@gmail.com>
2019-01-16 15:27:48 -0800

    Users can be removed from a role in Role Manager.

--- src/app/modules/role-manager/view-users/view-users.component.ts
+++ src/app/modules/role-manager/view-users/view-users.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import {RolesService} from '../../../services/roles.service';
 import {UserProfile} from '../../../models/user-profile.model';
+import {ModalComponent} from '../../display-elements/modal/modal.component';
+import {UsersService} from '../../../services/users.service';
+import {Role} from '../../../models/role.model';
+import {NotificationType} from '../../../notifier/notificiation.model';
+import {NotificationService} from '../../../notifier/notification.service';
 
 @Component({
   selector: 'app-view-users',
@@ -11,10 +16,17 @@ export class ViewUsersComponent implements OnInit {
 
   users: Array<UserProfile>;
   search: string;
+  activeUser: UserProfile;
 
-  @Input() activeRoleName: string;
+  //@Input() activeRoleName: string;
+  @Input() activeRole: Role;
 
-  constructor(private rolesSvc: RolesService) { 
+  @ViewChild('removeModal') private removeModal: ModalComponent;
+
+  constructor(
+    private rolesSvc: RolesService, 
+    private usersSvc: UsersService,
+    private notifySvc: NotificationService) { 
     this.users = new Array<UserProfile>();
     this.search = "";
   }
@@ -27,8 +39,34 @@ export class ViewUsersComponent implements OnInit {
     this.search = search;
   }
 
+  removeUser(user: UserProfile): void {
+    this.activeUser = user;
+    this.removeModal.show = true;
+  }
+
+  removeConfirmed(btnTitle: string): void {
+    this.removeModal.show = false;
+    this.usersSvc.deleteComposites(this.activeUser.id, [this.activeRole]).subscribe(
+      (response: any) => {
+        const index: number = this.users.findIndex((user: UserProfile) => user.id === this.activeUser.id);
+        this.users.splice(index, 1);
+        this.notifySvc.notify({
+          type: NotificationType.Success,
+          message: this.activeRole.name + " was removed from " + this.activeUser.username + " successfully"
+        });
+        this.usersSvc.userUpdated(this.activeUser);
+      },
+      (err: any) => {
+        this.notifySvc.notify({
+          type: NotificationType.Error,
+          message: "There was a problem while removing " + this.activeRole.name + " from " + this.activeUser.username
+        });
+      }
+    );
+  }
+
   private listUsers(): void {
-    this.rolesSvc.listUsers(this.activeRoleName).subscribe(
+    this.rolesSvc.listUsers(this.activeRole.name).subscribe(
       (users: Array<UserProfile>) => {
         this.users = users;
       },
