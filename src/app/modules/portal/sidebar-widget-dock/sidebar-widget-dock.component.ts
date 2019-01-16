b53c3f356bddc8d73b6b32a3efb1162f0524a4ef
Steven Chambers <steven.chambers@ultimateknowledge.com>
2019-01-16 10:25:55 -0500

    UI/Help Dock. Widget Navigation Fix

--- src/app/modules/portal/sidebar-widget-dock/sidebar-widget-dock.component.ts
+++ src/app/modules/portal/sidebar-widget-dock/sidebar-widget-dock.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-declare var $: any;
-
 @Component({
   selector: 'app-sidebar-widget-dock',
   templateUrl: './sidebar-widget-dock.component.html',
@@ -13,54 +11,4 @@ export class SidebarWidgetDockComponent implements OnInit {
 
   ngOnInit() {
   }
-
-  toggleSlideoutB(): void {
-    $('#slideout_innerB').toggleClass('expanded');
-  }
-
-  ngAfterViewInit() {
-    this.pageWrapperCollapseB(); 
-    this.logoAreaB(); 
-    this.overflowYB(); 
-    this.rmfWidgetsBgB(); 
-    this.closeWidget(); 
-    this.btnCloseSlider(); 
-  }
-
-  private pageWrapperCollapseB(): void {
-    $('#page-wrapper').click(() => {
-      $('#slideout_innerB').removeClass('expanded');
-    });
-  }
-
-  private logoAreaB(): void {
-    $('.logo-area').click(() => {
-      $('#slideout_innerB').removeClass('expanded');
-    });
-  }
-
-  private overflowYB(): void {
-    $('.overflow-y').click(() => {
-      $('#slideout_innerB').removeClass('expanded');
-    });
-  }
-
-  private rmfWidgetsBgB(): void {
-    $('.footer-icon-bg').click(() => {
-      $('#slideout_innerB').removeClass('expanded');
-    });
-  }
-
-  private closeWidget(): void {
-    $('.desktop-only-role').click(() => {
-      $('#slideout_innerB').removeClass('expanded');
-    });
-  }
-
-  private btnCloseSlider(): void {
-    $('.btnCloseSlider').click(() => {
-      $('#slideout_innerB').removeClass('expanded');
-    });
-  }
-
 }
--- src/app/modules/portal/sidebar-widgets/sidebar-widgets.component.ts
+++ src/app/modules/portal/sidebar-widgets/sidebar-widgets.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-sidebar-widgets',
   templateUrl: './sidebar-widgets.component.html',
@@ -12,9 +14,55 @@ export class SidebarWidgetsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
   }
 
-  
+  toggleSlideoutB(): void {
+    $('#slideout_innerB').toggleClass('expanded');
+  }
+
+  ngAfterViewInit() {
+    this.pageWrapperCollapseB(); 
+    this.logoAreaB(); 
+    this.overflowYB(); 
+    this.rmfWidgetsBgB(); 
+    this.closeWidget(); 
+    this.btnCloseSlider(); 
+  }
+
+  private pageWrapperCollapseB(): void {
+    $('#page-wrapper').click(() => {
+      $('#slideout_innerB').removeClass('expanded');
+    });
+  }
+
+  private logoAreaB(): void {
+    $('.logo-area').click(() => {
+      $('#slideout_innerB').removeClass('expanded');
+    });
+  }
+
+  private overflowYB(): void {
+    $('.overflow-y').click(() => {
+      $('#slideout_innerB').removeClass('expanded');
+    });
+  }
+
+  private rmfWidgetsBgB(): void {
+    $('.footer-icon-bg').click(() => {
+      $('#slideout_innerB').removeClass('expanded');
+    });
+  }
+
+  private closeWidget(): void {
+    $('.desktop-only-role').click(() => {
+      $('#slideout_innerB').removeClass('expanded');
+    });
+  }
+
+  private btnCloseSlider(): void {
+    $('.btnCloseSlider').click(() => {
+      $('#slideout_innerB').removeClass('expanded');
+    });
+  }
 
 }
