8bb95b12a57d85d0d7ad884acc4876772a1b7916
Steven M. Redman <steven.m.redman@gmail.com>
2019-02-14 12:55:24 -0800

    Realm-level role mappings for the micro-apps' client are now fetched and displayed.

--- src/app/modules/app-deployment/edit-app-role-mappings/edit-app-role-mappings.component.ts
+++ src/app/modules/app-deployment/edit-app-role-mappings/edit-app-role-mappings.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Role} from '../../../models/role.model';
+import {RoleWithPermissions} from '../../../models/role-with-permissions.model';
+import {ClientsService} from '../../../services/clients.service';
+import {RolesService} from '../../../services/roles.service';
+import {Cloner} from '../../../util/cloner';
 
 @Component({
   selector: 'app-edit-app-role-mappings',
@@ -7,9 +12,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditAppRoleMappingsComponent implements OnInit {
 
-  constructor() { }
+  rwps: Array<RoleWithPermissions>;
+
+  @Input() clientId: string;
+  @Input() clientName: string;
+
+  constructor(
+    private clientsSvc: ClientsService,
+    private rolesSvc: RolesService) { 
+      this.rwps = new Array<RoleWithPermissions>();
+    }
 
   ngOnInit() {
+    this.listRealmRoles();
+  }
+
+  private listRealmRoles(): void {
+    this.rolesSvc.list().subscribe(
+      (roles: Array<Role>) => {
+        this.listClientRoles(roles);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
+
+  private listClientRoles(realmRoles: Array<Role>): void {
+    this.clientsSvc.listRoles(this.clientId).subscribe(
+      (clientRoles: Array<Role>) => {
+        this.iterateClientComposites(realmRoles, clientRoles);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
+
+  private iterateClientComposites(realmRoles: Array<Role>, clientRoles: Array<Role>): void {
+    realmRoles.forEach((realmRole: Role) => {
+      this.rolesSvc.listClientComposites(realmRole.id, this.clientId).subscribe(
+        (composites: Array<Role>) => {
+          this.setPermissions(realmRole, clientRoles, composites);
+        },
+        (err: any) => {
+          console.log(err);
+        }
+      );
+    });
+  }
+
+  private setPermissions(realmRole: Role, clientRoles: Array<Role>, composites: Array<Role>): void {
+    if (composites.length) {
+      let permissions: Array<Role> = Cloner.cloneObjectArray<Role>(clientRoles);
+      permissions.forEach((p: Role) => {
+        const c: Role = composites.find((comp: Role) => comp.id === p.id);
+        if (c) {
+          p.active = true;
+        }
+      });
+      this.rwps.push({
+        role: realmRole,
+        permissions: permissions
+      });
+    }
   }
 
 }
