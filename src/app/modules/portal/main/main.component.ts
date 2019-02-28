e46e3231b9e5c3338dbabbbb7aadb647a33bdae0
Steven M. Redman <steven.m.redman@gmail.com>
2019-02-27 16:25:18 -0800

    Refactored the micro-app distribution method. The API call to get the list of user-accessible apps has been moved to the main component in the portal module. The sidebar-menu component now subscribes to an Observable that emits an event every time the app list is refreshed because of a role-mapping change or the interval-based app refresh. The sidebar-menu rebuilds the App/Widget list asynchronously whenever this event is emitted. Also fixed the redirectURI problem.

--- src/app/modules/portal/main/main.component.ts
+++ src/app/modules/portal/main/main.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, Validators, FormControl, FormBuilder} from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Subscription} from 'rxjs';
+import {App} from '../../../models/app.model';
+import {AppsService} from '../../../services/apps.service';
+import {AuthService} from '../../../services/auth.service';
+import {Router} from '@angular/router';
 
 declare var $: any;
 
@@ -8,11 +12,28 @@ declare var $: any;
  templateUrl: './main.component.html',
  styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private appUpdatedSub: Subscription;
+  private userUpdatedSub: Subscription;
+  private refreshInterval: any;
+
+  constructor(
+    private appsSvc: AppsService,
+    private authSvc: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.listUserApps();
+    this.subscribeToAppUpdates();
+    this.subscribeToUserUpdates();
+    this.setAppRefreshInterval();
+  }
+
+  ngOnDestroy() {
+    this.appUpdatedSub.unsubscribe();
+    this.userUpdatedSub.unsubscribe();
+    clearInterval(this.refreshInterval);
   }
 
   toggleNav(): void {
@@ -46,6 +67,61 @@ export class MainComponent implements OnInit {
     $('.skin-positionDark').click(() => {
       $('#skin-change').removeClass('contrast');
     });
+  }
+
+  private subscribeToAppUpdates(): void {
+    this.appUpdatedSub = this.appsSvc.appSub.subscribe(
+      (app: App) => {
+        this.listUserApps();
+      }
+    )
+  }
+
+  private subscribeToUserUpdates(): void {
+    this.userUpdatedSub = this.authSvc.sessionUpdatedSubject.subscribe(
+        (userId: string) => {
+          this.checkForRefreshByUserId(userId);
+        }
+    );
+  }
+
+  private checkForRefreshByUserId(userId: string): void {
+    if (userId === this.authSvc.getUserId()) {
+      this.listUserApps();
+    }
+  }
+
+  private setAppRefreshInterval(): void {
+    this.refreshInterval = setInterval(() => {
+      this.listUserApps();
+    }, 60000);
+  }
+
+  private listUserApps(): void {
+    this.appsSvc.listUserApps(this.authSvc.getUserId()).subscribe(
+      (apps: Array<App>) => {
+        console.log(apps);
+        this.appsSvc.cacheApps(apps);
+        this.verifyAppAccess(apps);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
+
+  private verifyAppAccess(apps: Array<App>): void {
+    const app: App = apps.find((app: App) => {
+      if (app.native) {
+        return window.location.href.indexOf(app.nativePath) !== -1;
+      }
+      else {
+        return window.location.href.indexOf(`app/${app.docId}`) !== -1;
+      }
+    });
+    if (!app && window.location.href.indexOf('/portal/dashboard') !== -1) {
+      this.router.navigateByUrl('/portal');
+    } 
   }
 
 }
--- src/app/modules/portal/sidebar-menu/sidebar-menu.component.ts
+++ src/app/modules/portal/sidebar-menu/sidebar-menu.component.ts
@@ -1,95 +1,22 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {AppsService} from '../../../services/apps.service';
 import {App} from '../../../models/app.model';
-import { Subscription } from 'rxjs';
-import {AuthService} from '../../../services/auth.service';
-import {Router} from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
   styleUrls: ['./sidebar-menu.component.scss']
 })
-export class SidebarMenuComponent implements OnInit, OnDestroy {
+export class SidebarMenuComponent implements OnInit {
 
-  apps: Array<App>;
-  private appUpdatedSub: Subscription;
-  private userUpdatedSub: Subscription;
-  private refreshInterval: any;
+  apps: Observable<Array<App>>;
 
-  constructor(
-    private appsSvc: AppsService, 
-    private authSvc: AuthService,
-    private router: Router) { 
-    this.apps = new Array<App>();
+  constructor(private appsSvc: AppsService) { 
+    
   }
 
   ngOnInit() {
-    this.listUserApps();
-    this.subscribeToAppUpdates();
-    this.subscribeToUserUpdates();
-    this.setAppRefreshInterval();
+    this.apps = this.appsSvc.appStoreSub.asObservable();
   }
-
-  ngOnDestroy() {
-    this.appUpdatedSub.unsubscribe();
-    this.userUpdatedSub.unsubscribe();
-    clearInterval(this.refreshInterval);
-  }
-
-  private subscribeToAppUpdates(): void {
-    this.appUpdatedSub = this.appsSvc.appSub.subscribe(
-      (app: App) => {
-        this.listUserApps();
-      }
-    )
-  }
-
-  private subscribeToUserUpdates(): void {
-    this.userUpdatedSub = this.authSvc.sessionUpdatedSubject.subscribe(
-        (userId: string) => {
-          this.checkForRefreshByUserId(userId);
-        }
-    );
-  }
-
-  private checkForRefreshByUserId(userId: string): void {
-    if (userId === this.authSvc.getUserId()) {
-      this.listUserApps();
-    }
-  }
-
-  private setAppRefreshInterval(): void {
-    this.refreshInterval = setInterval(() => {
-      this.listUserApps();
-    }, 60000);
-  }
-
-  private listUserApps(): void {
-    this.appsSvc.listUserApps(this.authSvc.getUserId()).subscribe(
-      (apps: Array<App>) => {
-        this.apps = apps;
-        this.appsSvc.cacheApps(apps);
-        this.verifyAppAccess();
-      },
-      (err: any) => {
-        console.log(err);
-      }
-    );
-  }
-
-  private verifyAppAccess(): void {
-    const app: App = this.apps.find((app: App) => {
-      if (app.native) {
-        return window.location.href.indexOf(app.nativePath) !== -1;
-      }
-      else {
-        return window.location.href.indexOf(`app/${app.docId}`) !== -1;
-      }
-    });
-    if (!app && window.location.href.indexOf('/portal/dashboard') !== -1) {
-      this.router.navigateByUrl('/portal');
-    } 
-  }
-
 }
