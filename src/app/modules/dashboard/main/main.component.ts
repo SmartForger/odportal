fd24af88482b32f3310ce13f4f11f925d33033df
Steven Chambers <steven.chambers@ultimateknowledge.com>
2019-03-01 14:55:26 -0500

    Update Dashboard Columns Rows Count

--- src/app/modules/dashboard/main/main.component.ts
+++ src/app/modules/dashboard/main/main.component.ts
@@ -44,10 +44,10 @@ export class MainComponent implements OnInit, OnDestroy {
       
     this.options = {
       gridType: 'fit',
-      minCols: 16,
-      minRows: 16,
-      defaultItemCols: 4,
-      defaultItemRows: 4,
+      minCols: 8,
+      minRows: 8,
+      defaultItemCols: 2,
+      defaultItemRows: 2,
       displayGrid: 'none',
       margin: 25,
       resizable: {
@@ -120,8 +120,8 @@ export class MainComponent implements OnInit, OnDestroy {
       parentAppTitle: app.appTitle,
       widgetTitle: widget.widgetTitle,
       gridsterItem: {
-        cols: 4,
-        rows: 4,
+        cols: 2,
+        rows: 2,
         x: 0,
         y: 0
       }
@@ -251,25 +251,25 @@ export class MainComponent implements OnInit, OnDestroy {
     this.dashboard.gridItems.push({
       parentAppTitle: 'Hardcoded Widgets App',
       widgetTitle: 'Active User Count',
-      gridsterItem: {rows: 4, cols: 4, x: 0, y: 0}
+      gridsterItem: {rows: 2, cols: 2, x: 0, y: 0}
     });
 
     this.dashboard.gridItems.push({
       parentAppTitle: 'Hardcoded Widgets App',
       widgetTitle: 'Pending User Count',
-      gridsterItem: {rows: 4, cols: 4, x: 0, y: 4}
+      gridsterItem: {rows: 2, cols: 2, x: 0, y: 2}
     });
 
     this.dashboard.gridItems.push({
       parentAppTitle: 'Hardcoded Widgets App',
       widgetTitle: 'User Chart (Active vs Pending)',
-      gridsterItem: {rows: 8, cols: 8, x: 4, y: 0}
+      gridsterItem: {rows: 4, cols: 4, x: 2, y: 0}
     });
 
     this.dashboard.gridItems.push({
       parentAppTitle: 'Hardcoded Widgets App',
       widgetTitle: 'User Count',
-      gridsterItem: {rows: 2, cols: 6, x: 0, y: 8}
+      gridsterItem: {rows: 2, cols: 3, x: 0, y: 4}
     });
   }
 
