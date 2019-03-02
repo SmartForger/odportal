d564dcbefe3b7ca7e93d1e7093405ff7ebcf0b2a
Steven M. Redman <steven.m.redman@gmail.com>
2019-03-01 19:55:52 -0800

    Forgot to save a file during the last commit.

--- src/app/models/widget-grid-item.model.ts
+++ src/app/models/widget-grid-item.model.ts
@@ -2,8 +2,8 @@ import { GridsterItem } from "angular-gridster2";
 
 export interface WidgetGridItem {
 
-    parentAppTitle: string;
-    widgetTitle: string;
+    parentAppTitle: string; //Will not be stored by the API
+    widgetTitle: string; //Will not be stored by the API
     widgetId?: string; //TODO make it required
     gridsterItem: GridsterItem;
 
