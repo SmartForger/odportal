d5536d85702d5e7d4c842fac04e2f3d594542904
Travis Larson <admin@travislarson.com>
2019-03-13 10:39:15 -0400

    Stable updates; material module and main scss

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,25 +3,27 @@ import { NgModule } from '@angular/core';
 import {
   MatButtonModule,
   MatMenuModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule
 } from '@angular/material';
+
+/** Initialize MDC Web components. */
+// const buttons = document.querySelectorAll('.mdc-button');
+// for (const button of buttons) {
+//   mdc.ripple.MDCRipple.attachTo(button);
+// }
+
+// const textFields = document.querySelectorAll('.mdc-text-field');
+// for (const textField of textFields) {
+//   mdc.textField.MDCTextField.attachTo(textField);
+// }
 
 @NgModule({
   imports: [
     MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
+    MatMenuModule
   ],
   exports: [
     MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
+    MatMenuModule
   ]
 })
 export class MaterialModule {}
