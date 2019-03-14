44a5f92342d33c3eed7af88f3547236a2f380cbe
Travis Larson <admin@travislarson.com>
2019-03-14 13:26:19 -0400

    Comment out bad instatiation in material.module.ts

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 
 	const menuEls = Array.from(document.querySelectorAll('.mdc-menu'));
 
-	menuEls.forEach((menuEl) => {
-	  // Initialize MDCSimpleMenu on each ".mdc-simple-menu"
-	  const menu = new mdc.menu.MDCMenu(menuEl);
+	// menuEls.forEach((menuEl) => {
+	//   // Initialize MDCSimpleMenu on each ".mdc-simple-menu"
+	//   const menu = new mdc.menu.MDCMenu(menuEl);
 
-	  // We wrapped menu and toggle into containers for easier selecting the toggles
-	  const dropdownToggle = menuEl.parentElement.querySelector('.js--dropdown-toggle');
-	  dropdownToggle.addEventListener('click', () => {
-	    menu.open = !menu.open;
-	  });
+	//   // We wrapped menu and toggle into containers for easier selecting the toggles
+	//   const dropdownToggle = menuEl.parentElement.querySelector('.js--dropdown-toggle');
+	//   dropdownToggle.addEventListener('click', () => {
+	//     menu.open = !menu.open;
+	//   });
 
-	  menu.setAnchorCorner(mdc.menu.MDCMenuFoundation.Corner.BOTTOM_START);
-	});
+	//   menu.setAnchorCorner(mdc.menu.MDCMenuFoundation.Corner.BOTTOM_START);
+	// });
 
 export class MaterialModule {}
