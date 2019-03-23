8c882f20bd6bffe069b6373dcc9461bb3c6f4c58
Steven M. Redman <steven.m.redman@gmail.com>
2019-03-23 16:24:24 -0700

    Added breadcrumbs to the app testing sandbox.

--- src/app/modules/app-manager/sandbox/sandbox.component.ts
+++ src/app/modules/app-manager/sandbox/sandbox.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {AppsService} from '../../../services/apps.service';
 import {App} from '../../../models/app.model';
+import {Breadcrumb} from '../../display-elements/breadcrumb.model';
+import {BreadcrumbsService} from '../../display-elements/breadcrumbs.service';
 
 @Component({
   selector: 'app-sandbox',
@@ -16,7 +18,8 @@ export class SandboxComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute, 
-    private appsSvc: AppsService) { 
+    private appsSvc: AppsService,
+    private crumbsSvc: BreadcrumbsService) { 
       this.showTools = true;
     }
 
@@ -28,11 +31,38 @@ export class SandboxComponent implements OnInit {
     this.appsSvc.fetch(this.route.snapshot.params['id']).subscribe(
       (app: App) => {
         this.app = app;
+        this.generateCrumbs();
       },
       (err: any) => {
         console.log(err);
       }
     );
+  }
+
+  private generateCrumbs(): void {
+    const crumbs: Array<Breadcrumb> = new Array<Breadcrumb>(
+      {
+        title: "Dashboard",
+        active: false,
+        link: '/portal'
+      },
+      {
+        title: "MicroApp Manager",
+        active: false,
+        link: '/portal/app-manager'
+      },
+      {
+        title: this.app.appTitle + " Details",
+        active: false,
+        link: `/portal/app-manager/edit/${this.app.docId}`
+      },
+      {
+        title: `${this.app.appTitle} Testing`,
+        active: true,
+        link: null
+      }
+    );
+    this.crumbsSvc.update(crumbs);
   }
 
 }
