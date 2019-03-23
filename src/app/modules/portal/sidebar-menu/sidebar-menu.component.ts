bb6ef2ea176b8d4c878775a1f5c7b5d56d23c5ef
Steven M. Redman <steven.m.redman@gmail.com>
2019-03-23 14:45:10 -0700

    Icons for third-party apps are now displayed in the menu. If a third-party app does not have an icon, the default icon is used.

--- src/app/modules/portal/sidebar-menu/sidebar-menu.component.ts
+++ src/app/modules/portal/sidebar-menu/sidebar-menu.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit} from '@angular/core';
 import {AppsService} from '../../../services/apps.service';
 import {App} from '../../../models/app.model';
 import { Observable } from 'rxjs';
+import {AuthService} from '../../../services/auth.service';
+import {UrlGenerator} from '../../../util/url-generator';
 
 @Component({
   selector: 'app-sidebar-menu',
@@ -12,11 +14,15 @@ export class SidebarMenuComponent implements OnInit {
 
   apps: Observable<Array<App>>;
 
-  constructor(private appsSvc: AppsService) { 
+  constructor(private appsSvc: AppsService, private authSvc: AuthService) { 
     
   }
 
   ngOnInit() {
     this.apps = this.appsSvc.appStoreSub.asObservable();
+  }
+
+  generateResourceURL(app: App): string {
+    return UrlGenerator.generateAppResourceUrl(this.authSvc.globalConfig.appsServiceConnection, app, app.appIcon);
   }
 }
