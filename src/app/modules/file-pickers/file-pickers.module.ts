1eeb9fa57b6de57fbdd79743a85cc012acbd29e8
Travis Larson <admin@travislarson.com>
2019-03-26 09:53:40 -0400

    Tweaks, adjustment to full sized widgets

--- src/app/modules/file-pickers/file-pickers.module.ts
+++ src/app/modules/file-pickers/file-pickers.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DragDropFilePickerComponent } from './drag-drop-file-picker/drag-drop-file-picker.component';
 
+import {MaterialModule} from '../../material.module';
+
 @NgModule({
   declarations: [DragDropFilePickerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [
     DragDropFilePickerComponent
