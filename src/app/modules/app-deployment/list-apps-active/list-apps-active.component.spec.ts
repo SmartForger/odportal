eb27b75fbe75affaaa35b1d3f3cbaae134395bba
Steven M. Redman <steven.m.redman@gmail.com>
2019-04-08 16:37:15 -0700

    Completed tests and documentation for list-apps-active.component.

--- src/app/modules/app-deployment/list-apps-active/list-apps-active.component.spec.ts
+++ src/app/modules/app-deployment/list-apps-active/list-apps-active.component.spec.ts
@@ -1,14 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
 
 import { ListAppsActiveComponent } from './list-apps-active.component';
+import {MatIconModule, MatIcon} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {App} from '../../../models/app.model';
 
-describe('ListAppsActiveComponent', () => {
+fdescribe('ListAppsActiveComponent', () => {
   let component: ListAppsActiveComponent;
   let fixture: ComponentFixture<ListAppsActiveComponent>;
 
+  const fakeApps: Array<App> = new Array<App>(
+    {
+      docId: "fake-app-one",
+      appTitle: "Fake App One",
+      enabled: true,
+      native: false,
+      approved: true,
+      vendorId: "fake-vendor-id",
+      clientId: "fake-client-one",
+      clientName: "fake client one",
+      version: "1.0.0",
+      widgets: []
+    },
+    {
+      docId: "fake-app-two",
+      appTitle: "Fake App Two",
+      enabled: false,
+      native: false,
+      approved: false,
+      vendorId: "fake-vendor-id",
+      clientId: "fake-client-one",
+      clientName: "fake client one",
+      version: "1.1.1",
+      widgets: []
+    }
+  );
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListAppsActiveComponent ]
+      declarations: [ 
+        ListAppsActiveComponent 
+      ],
+      imports: [
+        MatIconModule,
+        MatTooltipModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +61,22 @@ describe('ListAppsActiveComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should list apps and show an icon representing its enabled status for each app', async(() => {
+    expect(component.apps.length).toBe(0);
+    component.apps = fakeApps;
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      let trs = fixture.debugElement.queryAll(By.css('tbody tr'));
+      expect(trs.length).toBe(2);
+      let enabledIcon = trs[0].query(By.css('.color-green'));
+      expect(enabledIcon).toBeTruthy();
+      let disabledIcon = trs[0].query(By.css('.color-red'));
+      expect(disabledIcon).toBeNull();
+      enabledIcon = trs[1].query(By.css('.color-green'));
+      expect(enabledIcon).toBeNull();
+      disabledIcon = trs[1].query(By.css('.color-red'));
+      expect(disabledIcon).toBeTruthy();
+    });
+  }));
 });
--- src/app/modules/app-deployment/list-apps-active/list-apps-active.component.ts
+++ src/app/modules/app-deployment/list-apps-active/list-apps-active.component.ts
@@ -1,3 +1,8 @@
+/**
+ * @description Lists active vendor apps in a table. Shows which apps are enabled.
+ * @author Steven M. Redman
+ */
+
 import { Component, OnInit, Input } from '@angular/core';
 import {App} from '../../../models/app.model';
 
