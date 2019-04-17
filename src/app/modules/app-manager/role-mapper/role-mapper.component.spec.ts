f99a6a257c479f19d76a71dc68e83069411fb875
Steven M. Redman <steven.m.redman@gmail.com>
2019-04-17 11:24:05 -0700

    Added mock models for testing.

--- src/app/modules/app-manager/role-mapper/role-mapper.component.spec.ts
+++ src/app/modules/app-manager/role-mapper/role-mapper.component.spec.ts
@@ -6,10 +6,57 @@ import { RoleMapperComponent } from './role-mapper.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatChipsModule} from '@angular/material/chips';
 import {MatDialogModule} from '@angular/material/dialog';
+import {RolesService} from '../../../services/roles.service';
+import {ClientsService} from '../../../services/clients.service';
+import {AppsService} from '../../../services/apps.service';
+import {RoleWithPermissions} from '../../../models/role-with-permissions.model';
+import {Role} from '../../../models/role.model';
+import {App} from '../../../models/app.model';
 
 fdescribe('RoleMapperComponent', () => {
   let component: RoleMapperComponent;
   let fixture: ComponentFixture<RoleMapperComponent>;
+  let rolesSvc: RolesService;
+  let clientsSvc: ClientsService;
+  let appsSvc: AppsService;
+
+  const fakeRoles: Array<Role> = new Array<Role>(
+    {
+      id: "role-one-id",
+      name: "Role One",
+      description: "",
+      containerId: null,
+      composite: true,
+      clientRole: false
+    },
+    {
+      id: "role-two-id",
+      name: "Role Two",
+      description: "",
+      containerId: null,
+      composite: true,
+      clientRole: false
+    }
+  );
+
+  const fakeApp: App = {
+    docId: "fake-app-id",
+    appTitle: "Fake App",
+    enabled: true,
+    native: true,
+    clientId: "fake-client-id",
+    clientName: "fake-client-name",
+    roles: [
+      "role-one-id"
+    ],
+    externalPermissions: [
+      {
+        clientName: "test-client-name",
+        clientId: "test-client",
+        readPermission: "Read"
+      }
+    ]
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
