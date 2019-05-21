c89977c1c3d6df37fa7864200137c040675ab417
James <james.marcu@yahoo.com>
2019-05-21 12:53:11 -0700

    Fix for my fixes. Changed the way I wrap the cdr call in widget modal.

--- src/app/modules/portal/widget-modal/widget-modal.component.ts
+++ src/app/modules/portal/widget-modal/widget-modal.component.ts
@@ -83,9 +83,12 @@ export class WidgetModalComponent implements OnInit {
     return this._hidden;
   }
 
-  filterWidgets(): boolean{
+  updateFilter(): void{
+    this.cdr.detectChanges();
+  }
+
+  filterWidget(title: string): boolean{
     if(this.searchBar.nativeElement.value){
-      let title = this.searchBar.nativeElement.value;
       return title.toLowerCase().includes(this.searchBar.nativeElement.value.toLowerCase());
     }
     else{
