c1a56ce221317ed1b2eadb6b7b81de74f02a36a5
SmartForger <23446619+SmartForger@users.noreply.github.com>
2019-05-31 12:58:05 -0700

    add verification routes

--- src/app/modules/verification-manager/verification-manager.module.ts
+++ src/app/modules/verification-manager/verification-manager.module.ts
@@ -4,23 +4,50 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Modules
 import { MaterialModule } from '../../material.module';
+import { DisplayElementsModule } from '../display-elements/display-elements.module';
+import { ListFiltersModule } from '../list-filters/list-filters.module';
 
 // Components
 import { MainComponent } from './main/main.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { UsersTableComponent } from './users-table/users-table.component';
 
 const ROUTES: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        component: ListComponent
+      },
+      {
+        path: 'users/:id',
+        component: DetailsComponent
+      },
+      {
+        path: '**',
+        redirectTo: '/'
+      }
+    ]
   }
 ];
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [
+    MainComponent,
+    ListComponent,
+    DetailsComponent,
+    UsersTableComponent
+  ],
   imports: [
     CommonModule,
+    DisplayElementsModule,
+    ListFiltersModule,
     MaterialModule,
     RouterModule.forChild(ROUTES)
   ]
 })
-export class VerificationManagerModule { }
+export class VerificationManagerModule {}
