b02045ce8886a0ed9663b6ed630e9283b3b9a7e9
Travis Larson <admin@travislarson.com>
2019-06-11 20:35:38 -0400

    UI updates for dynamic form, my registration modules

--- src/app/modules/dynamic-form/dynamic-form.module.ts
+++ src/app/modules/dynamic-form/dynamic-form.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material';
 import { MatButtonToggleModule } from '@angular/material';
 
@@ -13,6 +14,7 @@ import { InputElementsModule } from '../input-elements/input-elements.module';
   exports: [DynamicFormComponent],
   imports: [
     CommonModule, 
+    MatCardModule,
     MatButtonModule, 
     MatButtonToggleModule,
     FormElementsModule, 
