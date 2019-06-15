7c2a135b18394513f42c0d707e6bb820bf2f6f44
James <james.marcu@yahoo.com>
2019-06-14 17:11:26 -0700

    Added a password validator to the registration landing screen.

--- src/app/modules/registration-landing/registration-basic-info/registration-basic-info.component.ts
+++ src/app/modules/registration-landing/registration-basic-info/registration-basic-info.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormControl, Validators, FormBuilder, ValidatorFn, AbstractControl } from '@angular/forms';
 import { CustomForm } from '../../../base-classes/custom-form';
 import {AccountRepresentation} from '../../../models/account-representation.model';
 import {UserRepresentation} from '../../../models/user-representation.model';
@@ -44,7 +44,7 @@ export class RegistrationBasicInfoComponent extends CustomForm implements OnInit
       email: new FormControl('', [Validators.email, Validators.required]),
       username: new FormControl(''),
       password: new FormControl('', [Validators.required, Validators.maxLength(25)]),
-      confirmPassword: new FormControl('', [Validators.required, Validators.maxLength(25)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.maxLength(25), this.passwordRequirementsValidator()]),
     });
   }
 
@@ -108,6 +108,41 @@ export class RegistrationBasicInfoComponent extends CustomForm implements OnInit
         this.authSvc.login();
       }
     );
+  }
+
+  private passwordRequirementsValidator(): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      let invalid: boolean = true;
+      let pass: string = control.value;
+      if(pass.length >= 15){
+        let char: number = 0;
+
+        let uppers: number = 0;
+        let lowers: number = 0;
+        let specials: number = 0;
+        let numbers: number = 0;
+
+        while(invalid && char < pass.length){
+          if(pass.charAt(char).match('[A-Z]')){
+            uppers++;
+          }
+          else if(pass.charAt(char).match('[a-z]')){
+            lowers++;
+          }
+          else if(pass.charAt(char).match('[0-9]')){
+            numbers++;
+          }
+          else{
+            specials++;
+          }
+
+          invalid = (uppers < 2 || lowers < 2 || numbers < 2 || specials < 2);
+
+          char++;
+        }
+      }
+      return invalid ? {'passwordRequirements': {value: control.value}} : null;
+    };
   }
 
 }
