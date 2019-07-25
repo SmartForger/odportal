efb90841a3016260b3b8a2cf622855a981ea8e3c
Steven M. Redman <steven.m.redman@gmail.com>
2019-07-24 17:42:42 -0700

    Deleted because it is no longer needed.

--- src/app/models/widget-feedback-with-models.model.ts
+++
@@ -1,9 +0,0 @@
-import { App } from './app.model';
-import { Widget } from "./widget.model";
-import { AverageRating } from "./feedback-widget.model";
-
-export interface WidgetFeedbackWithModels{
-    app: App;
-    widget: Widget;
-    averageRating: AverageRating;
-}
