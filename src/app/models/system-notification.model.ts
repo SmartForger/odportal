62ee9ae7b5d4edd2638b0d0023535a9560f727f7
Steven M. Redman <steven.m.redman@gmail.com>
2019-08-02 19:24:21 -0700

    This file contains several models that support notification data-bindings and RESTful API calls.

---
+++ src/app/models/system-notification.model.ts
@@ -0,0 +1,51 @@
+export enum LaunchType {
+    MicroApp = 1,
+    Widget = 2
+}
+
+export enum Priority {
+    Critical = 1,
+    HighPriority = 2,
+    LowPriority = 3,
+    Passive = 4
+}
+
+export enum IconType {
+    Image = "image",
+    Icon = "icon"
+}
+
+export interface SystemNotification {
+
+    docId?: string;
+    type?: string;
+    subject: string;
+    message: string;
+    createdAt?: string;
+    icon?: {
+        type: IconType,
+        source: string
+    },
+    priority: Priority,
+    target?: {
+        users: Array<string>,
+        roles: Array<string>
+    },
+    launch?: {
+        type: LaunchType,
+        id: string,
+        state: Object
+    }
+    timestamp?: string; //local use only
+
+}
+
+export interface ReadReceipt {
+
+    docId?: string;
+    type?: string;
+    notificationId: string;
+    userId?: string;
+    createdAt?: string;
+
+}
