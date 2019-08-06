ed224165ffb518cb3c4285b7bd87c0ae73ea080a
Steven M. Redman <steven.m.redman@gmail.com>
2019-08-05 17:46:49 -0700

    Removed console output.

--- src/app/services/widget-tracker.service.ts
+++ src/app/services/widget-tracker.service.ts
@@ -13,13 +13,11 @@ export class WidgetTrackerService {
 
   add(widgetId: string): void {
     this.widgetIds.push(widgetId);
-    console.log(this.widgetIds.length);
   }
 
   remove(widgetId: string): void {
     const index: number = this.widgetIds.findIndex((id: string) => id === widgetId);
     this.widgetIds.splice(index, 1);
-    console.log(this.widgetIds.length);
   }
 
   exists(widgetId: string): boolean {
