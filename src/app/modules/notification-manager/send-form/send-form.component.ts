5816355593b86c339ad98a46358f7850e79acbf4
Steven M. Redman <steven.m.redman@gmail.com>
2019-08-09 19:08:28 -0700

    Added new components for choosing roles and users.

--- src/app/modules/notification-manager/send-form/send-form.component.ts
+++ src/app/modules/notification-manager/send-form/send-form.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {CustomForm} from '../../../base-classes/custom-form';
 import {FormControl, Validators, FormBuilder} from '@angular/forms';
 import {KeyValue} from '../../../models/key-value.model';
 import {Priority} from '../../../models/system-notification.model';
+import {Role} from '../../../models/role.model';
+import {UserProfile} from '../../../models/user-profile.model';
 
 @Component({
   selector: 'app-send-form',
@@ -12,8 +14,10 @@ import {Priority} from '../../../models/system-notification.model';
 export class SendFormComponent extends CustomForm implements OnInit {
 
   priorities: Array<KeyValue>;
+  roleNames: Array<string>;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(
+    private formBuilder: FormBuilder) { 
     super();
     this.priorities = new Array<KeyValue>(
       {
@@ -33,17 +37,22 @@ export class SendFormComponent extends CustomForm implements OnInit {
         value: Priority.Passive.toString()
       }
     );
+    this.roleNames = new Array<string>();
   }
 
   ngOnInit() {
     this.buildForm();
   }
 
+  rolesUpdated(roleNames: Array<string>): void {
+    this.roleNames = roleNames;
+  }
+
   protected buildForm(): void {
     this.form = this.formBuilder.group({
       subject: new FormControl('', [Validators.maxLength(250), Validators.required]),
       message: new FormControl('', [Validators.maxLength(500), Validators.required]),
-      priority: new FormControl('')
+      priority: new FormControl("4")
     });
   }
 
