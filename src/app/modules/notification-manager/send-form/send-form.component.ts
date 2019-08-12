3f92d94658228bca90db6cd06d2c3388bbb7e6b4
Steven M. Redman <steven.m.redman@gmail.com>
2019-08-12 11:05:56 -0700

    Added method for creating a SystemNotification model from the form submission. This model is emitted to the parent.

--- src/app/modules/notification-manager/send-form/send-form.component.ts
+++ src/app/modules/notification-manager/send-form/send-form.component.ts
@@ -3,8 +3,7 @@ import {CustomForm} from '../../../base-classes/custom-form';
 import {FormControl, Validators, FormBuilder} from '@angular/forms';
 import {KeyValue} from '../../../models/key-value.model';
 import {Priority} from '../../../models/system-notification.model';
-import {Role} from '../../../models/role.model';
-import {UserProfile} from '../../../models/user-profile.model';
+import {SystemNotification, IconType} from '../../../models/system-notification.model';
 
 @Component({
   selector: 'app-send-form',
@@ -15,6 +14,7 @@ export class SendFormComponent extends CustomForm implements OnInit {
 
   priorities: Array<KeyValue>;
   roleNames: Array<string>;
+  userIds: Array<string>;
 
   constructor(
     private formBuilder: FormBuilder) { 
@@ -38,6 +38,7 @@ export class SendFormComponent extends CustomForm implements OnInit {
       }
     );
     this.roleNames = new Array<string>();
+    this.userIds = new Array<string>();
   }
 
   ngOnInit() {
@@ -48,12 +49,33 @@ export class SendFormComponent extends CustomForm implements OnInit {
     this.roleNames = roleNames;
   }
 
+  usersUpdated(userIds: Array<string>): void {
+    this.userIds = userIds;
+  }
+
   protected buildForm(): void {
     this.form = this.formBuilder.group({
       subject: new FormControl('', [Validators.maxLength(250), Validators.required]),
       message: new FormControl('', [Validators.maxLength(500), Validators.required]),
       priority: new FormControl("4")
     });
+  }
+
+  submitForm(formData: {subject: string, message: string, priority: string}): void {
+    const notification: SystemNotification = {
+      subject: formData.subject,
+      message: formData.message,
+      priority: parseInt(formData.priority),
+      target: {
+        users: this.userIds,
+        roles: this.roleNames
+      },
+      icon: {
+        type: IconType.Icon,
+        source: "notification_important"
+      }
+    };
+    this.formSubmitted.emit(notification);
   }
 
 }
