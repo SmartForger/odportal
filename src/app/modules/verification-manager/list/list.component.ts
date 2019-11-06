a169fb88c45d2ced47be9e3b1e79b6d48ba1868e
James Marcu <james.marcu@yahoo.com>
2019-11-05 22:38:37 -0800

    Temporary patch for verification manager to work with the mid-update applicant table.

--- src/app/modules/verification-manager/list/list.component.ts
+++ src/app/modules/verification-manager/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserProfileWithRegistration } from 'src/app/models/user-profile-with-registration.model';
 import { Router } from '@angular/router';
 import { VerificationService } from 'src/app/services/verification.service';
+import { UserRegistrationSummary } from 'src/app/models/user-registration-summary.model';
 
 @Component({
   selector: 'app-list',
@@ -9,14 +10,28 @@ import { VerificationService } from 'src/app/services/verification.service';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent{
-  users: Array<UserProfileWithRegistration>;
+  users: Array<UserRegistrationSummary>;
 
   constructor(private router: Router, private verSvc: VerificationService) { 
-    this.users = new Array<UserProfileWithRegistration>();
+    this.users = new Array<UserRegistrationSummary>();
   }
 
   ngOnInit(){
-    this.verSvc.getUsersToApprove().subscribe((users: Array<UserProfileWithRegistration>) => {this.users = users})
+    this.verSvc.getUsersToApprove().subscribe((users: Array<UserProfileWithRegistration>) => {
+        console.log(users);
+        let summaries = new Array<UserRegistrationSummary>();
+        users.forEach((regWithProfile: UserProfileWithRegistration) => {
+            summaries.push({
+                docId: regWithProfile.docId,
+                status: null,
+                userProfile: regWithProfile.userProfile,
+                registrationId: null,
+                registrationTitle: null
+            });
+        });
+        this.users = summaries;
+        console.log(this.users);
+    })
   }
 
   details(user: UserProfileWithRegistration): void{
