4cc3a30711b7c1dde668b22a1be164159fe7d415
SmartForger <23446619+SmartForger@users.noreply.github.com>
2020-01-20 17:44:55 -0800

    add breadcrumbs to notification manager

--- src/app/modules/notification-manager/main/main.component.ts
+++ src/app/modules/notification-manager/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Breadcrumb } from "../../display-elements/breadcrumb.model";
+import { BreadcrumbsService } from "../../display-elements/breadcrumbs.service";
 
 @Component({
   selector: 'app-main',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private crumbsSvc: BreadcrumbsService) { }
 
   ngOnInit() {
+    this.generateCrumbs();
   }
 
+  private generateCrumbs(): void {
+    const crumbs: Array<Breadcrumb> = new Array<Breadcrumb>(
+      {
+        title: "Dashboard",
+        active: false,
+        link: "/portal"
+      },
+      {
+        title: "Notification Manager",
+        active: true,
+        link: null
+      }
+    );
+    this.crumbsSvc.update(crumbs);
+  }
 }
