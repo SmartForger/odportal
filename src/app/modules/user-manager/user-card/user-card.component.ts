c79d137af16ddcb71cc1c6474d4ee5202e7adc6d
SmartForger <23446619+SmartForger@users.noreply.github.com>
2020-01-23 16:37:09 -0500

    add card view to user manager

---
+++ src/app/modules/user-manager/user-card/user-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { UserProfile } from '../../../models/user-profile.model';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.scss']
+})
+export class UserCardComponent implements OnInit {
+  @Input() user: UserProfile;
+
+  constructor() {
+    this.user = {
+      firstName: '',
+      lastName: '',
+      username: '',
+      email: ''
+    };
+  }
+
+  ngOnInit() {
+  }
+
+}
