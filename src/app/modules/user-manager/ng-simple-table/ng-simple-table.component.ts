1d70c0d6340f2497ffe7cb7c377831977d9c9a30
SmartForger <23446619+SmartForger@users.noreply.github.com>
2020-01-29 18:46:56 -0800

    add simspace to user manager

--- src/app/modules/user-manager/ng-simple-table/ng-simple-table.component.ts
+++ src/app/modules/user-manager/ng-simple-table/ng-simple-table.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { SimpleTableColumn, SimpleTableActionEvent } from 'src/app/models/simple-table-column.model';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import {
+  SimpleTableColumn,
+  SimpleTableActionEvent,
+  SimpleTableRow,
+  SimpleTableCellData
+} from "src/app/models/simple-table-column.model";
 
 @Component({
-  selector: 'ng-simple-table',
-  templateUrl: './ng-simple-table.component.html',
-  styleUrls: ['./ng-simple-table.component.scss']
+  selector: "ng-simple-table",
+  templateUrl: "./ng-simple-table.component.html",
+  styleUrls: ["./ng-simple-table.component.scss"]
 })
 export class NgSimpleTableComponent implements OnInit {
   @Input() tableTitle: string;
   @Input() columns: SimpleTableColumn[];
-  @Input() data: any[];
+  @Input() data: SimpleTableRow[];
   @Output() actionClick: EventEmitter<SimpleTableActionEvent>;
   @Input() tableButtonLabel: string;
   @Output() tableButtonClick: EventEmitter<any>;
+
+  private readonly circleColors = {
+    red: "#CB1010",
+    blue: "#0063BF",
+    green: "#04B44B"
+  };
 
   constructor() {
     this.columns = [];
@@ -21,13 +32,20 @@ export class NgSimpleTableComponent implements OnInit {
     this.tableButtonClick = new EventEmitter<any>();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   handleAction(action, row) {
     this.actionClick.emit({
       action,
       row
     });
+  }
+
+  getCircleColor(row: SimpleTableRow, col: SimpleTableColumn): string {
+    return (
+      this.circleColors[
+        (row[col.field] as SimpleTableCellData).color || col.color
+      ] || "#000"
+    );
   }
 }
