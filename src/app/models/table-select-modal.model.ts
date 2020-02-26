015e85466e089d58b408ec0d3678da6f507080a4
James Marcu <james.marcu@yahoo.com>
2020-02-26 14:27:42 -0800

    Various fixes for user tables.

--- src/app/models/table-select-modal.model.ts
+++ src/app/models/table-select-modal.model.ts
@@ -1,8 +1,11 @@
-export interface TableSelectModalModel {
-  title: string;
-  searchPlaceholder?: string;
+import { Observable } from "rxjs";
+
+export interface TableSelectModalModel<T> {
   buttonLabel?: string;
   columns: string[];
   data: any[];
-  filterFunc: (search: string, item: any) => boolean;
+  filterFunc: (search: string, data: Array<T>) => Array<T>;
+  query: (first: number, max: number) => Observable<Array<T>>
+  searchPlaceholder?: string;
+  title: string;
 }
--- src/app/modules/display-elements/table-select-modal/table-select-modal.component.ts
+++ src/app/modules/display-elements/table-select-modal/table-select-modal.component.ts
@@ -2,85 +2,110 @@ import { Component, OnInit, Inject } from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef, Sort } from "@angular/material";
 import { PlatformModalType } from "src/app/models/platform-modal.model";
 import { TableSelectModalModel } from "src/app/models/table-select-modal.model";
+import { DirectQueryList } from "src/app/base-classes/direct-query-list";
+import { UserProfile } from "src/app/models/user-profile.model";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: "app-table-select-modal",
-  templateUrl: "./table-select-modal.component.html",
-  styleUrls: ["./table-select-modal.component.scss"]
+    selector: "app-table-select-modal",
+    templateUrl: "./table-select-modal.component.html",
+    styleUrls: ["./table-select-modal.component.scss"]
 })
-export class TableSelectModalComponent implements OnInit {
-  tableData: any[];
-  filteredData: any[];
-  currentSort: Sort;
-  selectedData: any[];
-  search: string;
+export class TableSelectModalComponent<T> extends DirectQueryList<T> implements OnInit {
 
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: TableSelectModalModel,
-    private dlgRef: MatDialogRef<TableSelectModalComponent>
-  ) {
-    this.dlgRef.addPanelClass("platform-modal");
-    this.dlgRef.addPanelClass(PlatformModalType.PRIMARY);
+    currentSort: Sort;
+    filteredData: Array<T>;
+    filteredSelectedData: Array<T>;
+    selectedData: Array<T>;
+    search: string;
 
-    this.selectedData = [];
-    this.filteredData = data.data;
-    this.tableData = data.data;
-    this.currentSort = {
-      active: data.columns[0],
-      direction: "asc"
-    };
-    this.search = "";
-  }
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: TableSelectModalModel<T>,
+        private dlgRef: MatDialogRef<TableSelectModalComponent<T>>
+    ) {
+        super(data.columns);
 
-  ngOnInit() {}
+        this.dlgRef.addPanelClass("platform-modal");
+        this.dlgRef.addPanelClass(PlatformModalType.PRIMARY);
 
-  filterItems(str) {
-    this.search = str.toLowerCase();
-    this.filter();
-  }
-
-  filter() {
-    const filterFunc = item =>
-      this.data.filterFunc(this.search, item) && this.selectedData.indexOf(item) < 0;
-    this.filteredData = this.data.data.filter(filterFunc);
-    this.sortChange();
-  }
-
-  sortChange(sort?: Sort) {
-    if (sort) {
-      this.currentSort = sort;
+        this.currentSort = {
+            active: data.columns[0],
+            direction: "asc"
+        };
+        this.displayItems = data.data;
+        this.filteredData = data.data;
+        this.filteredSelectedData = new Array<T>();
+        this.query = data.query;
+        this.search = "";
+        this.selectedData = new Array<T>();
     }
 
-    const toString = val => (val ? "" + val : "");
-
-    this.filteredData.sort((a: any, b: any) => {
-      let valA, valB;
-      if (this.currentSort.active === "fullname") {
-        valA = toString(a.firstName) + " " + toString(a.lastName);
-        valB = toString(b.firstName) + " " + toString(b.lastName);
-      } else if (["username"].indexOf(this.currentSort.active) >= 0) {
-        valA = toString(a[this.currentSort.active]);
-        valB = toString(b[this.currentSort.active]);
-      }
-
-      return this.currentSort.direction === "asc"
-        ? valA.localeCompare(valB)
-        : -valA.localeCompare(valB);
-    });
-    this.tableData = [...this.selectedData, ...this.filteredData];
-  }
-
-  select(row) {
-    const i = this.selectedData.indexOf(row);
-    if (i >= 0) {
-      this.selectedData.splice(i, 1);
-    } else {
-      this.selectedData.push(row);
+    filter(str) {
+        this.search = str.toLowerCase();
+        this.filterItems();
     }
-    this.filter();
-  }
 
-  submit() {
-    this.dlgRef.close(this.selectedData);
-  }
+    filterItems() {
+        let allFilteredData = this.data.filterFunc(this.search, this.data.data);
+        console.log('allFilteredData: ...');
+        console.log(allFilteredData);
+        this.filteredData = new Array<T>();
+        this.filteredSelectedData = new Array<T>();
+        allFilteredData.forEach((data: T) => {
+            let foundInSelected = this.selectedData.find((selectedValue) => {return data === selectedValue;});
+            if(foundInSelected !== undefined){
+                this.filteredSelectedData.push(data);
+            }
+            else{
+                this.filteredData.push(data);
+            }
+        });
+
+        console.log('filteredSelectedData: ...');
+        console.log(this.filteredSelectedData);
+        console.log('filteredData: ...');
+        console.log(this.filteredData);
+
+        this.sortChange();
+        this.table.renderRows();
+    }
+
+    select(row) {
+        const i = this.selectedData.indexOf(row);
+        if (i >= 0) {
+            this.selectedData.splice(i, 1);
+        } else {
+            this.selectedData.push(row);
+        }
+        this.filterItems();
+    }
+
+    sortChange(sort?: Sort) {
+        if (sort) {
+            this.currentSort = sort;
+        }
+
+        const toString = val => (val ? "" + val : "");
+
+        this.filteredSelectedData.sort((a: any, b: any) => {
+            let valA, valB;
+            if (this.currentSort.active === "fullname") {
+                valA = toString(a.firstName) + " " + toString(a.lastName);
+                valB = toString(b.firstName) + " " + toString(b.lastName);
+            } else if (["username"].indexOf(this.currentSort.active) >= 0) {
+                valA = toString(a[this.currentSort.active]);
+                valB = toString(b[this.currentSort.active]);
+            }
+
+            return this.currentSort.direction === "asc"
+                ? valA.localeCompare(valB)
+                : -valA.localeCompare(valB);
+        });
+        this.filteredItems = [...this.filteredSelectedData, ...this.filteredData];
+        this.listDisplayItems();
+    }
+
+    submit() {
+        this.dlgRef.close(this.selectedData);
+    }
 }
