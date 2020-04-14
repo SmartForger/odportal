2d16c69b7871b7183a3ce85f8b7d95704087d707
James Marcu <james.marcu@yahoo.com>
2020-04-14 15:46:04 -0700

    Security & Access UI/UX

--- src/app/modules/user-manager/security-and-access/security-and-access.component.ts
+++ src/app/modules/user-manager/security-and-access/security-and-access.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSession } from 'src/app/models/user-session';
 import { DynamicallyRenderable } from 'src/app/interfaces/dynamically-renderable';
+import { UserProfile } from 'src/app/models/user-profile.model';
 
 @Component({
     selector: 'app-security-and-access',
@@ -9,11 +10,14 @@ import { DynamicallyRenderable } from 'src/app/interfaces/dynamically-renderable
 })
 export class SecurityAndAccessComponent implements DynamicallyRenderable, OnInit {
 
+    currentSession: UserSession;
+    profile: UserProfile;
     sessions: Array<UserSession>;
 
     readonly DISPLAYED_COLUMNS = ['address', 'geolocation', 'action']
 
     constructor() {
+        this.currentSession = {id: '', username: '', userId: '', clientId: '', ipAddress: '192.168.5.42', start: 0, lastAccess: 0, geolocation: 'Orlando, Florida, US'};
         this.sessions = [
             {id: '', username: '', userId: '', clientId: '', ipAddress: '192.168.5.42', start: 0, lastAccess: 0, geolocation: 'Orlando, Florida, US'},
             {id: '', username: '', userId: '', clientId: '', ipAddress: '50.54.30.255', start: 0, lastAccess: 0, geolocation: 'Cleveland, Ohio, US'}
@@ -23,6 +27,14 @@ export class SecurityAndAccessComponent implements DynamicallyRenderable, OnInit
     ngOnInit() { }
 
     setState(state: any): void{
-        
+        this.profile = state;
     };
+
+    terminate(session: UserSession): void{
+
+    }
+
+    terminateAll(): void{
+        
+    }
 }
