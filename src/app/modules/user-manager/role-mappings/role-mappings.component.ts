51ef21fe8acf4f2df5bc87def0d906867f319999
James Marcu <james.marcu@yahoo.com>
2020-04-15 13:34:32 -0700

    Implemented client permissions for role maappings.

--- src/app/modules/user-manager/role-mappings/role-mappings.component.ts
+++ src/app/modules/user-manager/role-mappings/role-mappings.component.ts
@@ -1,29 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DynamicallyRenderable } from 'src/app/interfaces/dynamically-renderable';
 import { UserProfile } from 'src/app/models/user-profile.model';
-import { RolesService } from 'src/app/services/roles.service';
 import { Role } from 'src/app/models/role.model';
 import { UsersService } from 'src/app/services/users.service';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Subscription } from 'rxjs';
+import { AppPermissionsBroker } from 'src/app/util/app-permissions-broker';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
     selector: 'app-role-mappings',
     templateUrl: './role-mappings.component.html',
     styleUrls: ['./role-mappings.component.scss']
 })
-export class RoleMappingsComponent implements DynamicallyRenderable, OnInit {
+export class RoleMappingsComponent implements DynamicallyRenderable, OnDestroy, OnInit {
 
+    canManage: boolean;
+    hasRole: Array<boolean>;
     get profile(): UserProfile{return this._profile;}
     set profile(profile: UserProfile){this.setProfile(profile);}
     private _profile: UserProfile;
     roles: Array<Role>;
-    hasRole: Array<boolean>;
 
-    constructor(private userSvc: UsersService) {
+    private broker: AppPermissionsBroker;
+    private sessionUpdateSub: Subscription;
+    private readonly APP_ID = 'role-mappings';
+
+    constructor(
+        private authSvc: AuthService,
+        private userSvc: UsersService
+    ) {
+        this.broker = new AppPermissionsBroker(this.APP_ID);
         this.roles = new Array<Role>();
     }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.canManage = this.broker.hasPermission("manage");
+        this.subscribeToSessionUpdate();
+    }
+
+    ngOnDestroy() {
+        if(this.sessionUpdateSub){
+            this.sessionUpdateSub.unsubscribe();
+        }
+    }
 
     setState(state: any): void{
         this.profile = state;
@@ -33,18 +52,44 @@ export class RoleMappingsComponent implements DynamicallyRenderable, OnInit {
         this.hasRole[roleIndex] = !this.hasRole[roleIndex];
     }
 
-    private setProfile(profile: UserProfile): void{
+    private loadRoles(): void{
         forkJoin(
-            this.userSvc.listAvailableRoles(profile.userId),
-            this.userSvc.listAssignedRoles(profile.userId)
+            this.userSvc.listAvailableRoles(this.profile.userId),
+            this.userSvc.listAssignedRoles(this.profile.userId)
         ).subscribe((results: Array<Array<Role>>) => {
-            this._profile = profile;
-            this.roles = Array.from(results[0]).concat(results[1]).sort((a: Role, b: Role) => {return a.name.localeCompare(b.name);});
-            this.hasRole = new Array();
-            this.roles.forEach((role: Role, index: number) => {
-                let assigned = results[1].find((assignedRole: Role) => {return assignedRole.id === role.id;});
-                this.hasRole[index] = assigned !== undefined;
-            });
+            if(this.canManage){
+                this.roles = Array.from(results[0]).concat(results[1]).sort((a: Role, b: Role) => {return a.name.localeCompare(b.name);});
+                this.hasRole = new Array();
+                this.roles.forEach((role: Role, index: number) => {
+                    let assigned = results[1].find((assignedRole: Role) => {return assignedRole.id === role.id;});
+                    this.hasRole[index] = assigned !== undefined;
+                });
+            }
+            else{
+                this.roles = results[1].sort((a: Role, b: Role) => {return a.name.localeCompare(b.name);});;
+                this.hasRole = new Array();
+                this.roles.forEach((r: Role) => {this.hasRole.push(true);});
+                console.log('roles: ...', this.roles);
+                console.log('hasRole: ...', this.hasRole);
+            }
+        });
+    }
+
+    private setProfile(profile: UserProfile): void{
+        this._profile = profile;
+        this.loadRoles();
+    }
+
+    private subscribeToSessionUpdate(): void {
+        this.sessionUpdateSub = this.authSvc.observeUserSessionUpdates().subscribe((userId: string) => {
+            console.log('sessionUpdate userId: ', userId);
+            if (userId === this.authSvc.getUserId()) {
+                console.log('they are equal');
+                this.canManage = this.broker.hasPermission("manage");
+                if(this.profile !== undefined){
+                    this.loadRoles();
+                }
+            }
         });
     }
 }
